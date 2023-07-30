4e3eb67c2ad9d73fc3c9ca70202305d6a43cdbbf
Deriksl <133131434+Deriksl@users.noreply.github.com>
2023-07-30 12:40:42 -0600

    Add files via upload

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,404 @@
+#include <iostream>
+#include <stack>
+
+template<typename T>
+class BinaryTree
+{
+public: // Corregido: Declarar Node como public para que Predecessor tenga acceso.
+    template <typename U>
+    struct Node
+    {
+    public:
+        Node(U in_data, Node<U>* in_parent, Node<U>* in_left = nullptr, Node<U>* in_right = nullptr) :
+            data(in_data), parent(in_parent), left(in_left), right(in_right)
+        {
+        }
+
+        ~Node()
+        {
+        }
+
+        U data;
+        Node<U>* parent;
+        Node<U>* left;
+        Node<U>* right;
+    };
+
+    Node<T>* root;
+
+public:
+    BinaryTree() : root(nullptr)
+    {
+    }
+
+    ~BinaryTree()
+    {
+        while (root != nullptr)
+        {
+            DeleteNode(root);
+        }
+    }
+
+    // Función para obtener el nodo raíz del árbol.
+    Node<T>* GetRoot()
+    {
+        return root;
+    }
+
+    // Función para encontrar el nodo con el valor mínimo desde un nodo dado.
+    Node<T>* MinimumFromNode(Node<T>* in_root)
+    {
+        Node<T>* temp = in_root;
+        while (temp->left != nullptr)
+        {
+            temp = temp->left;
+        }
+        return temp;
+    }
+
+    // Función para encontrar el nodo con el valor mínimo en todo el árbol.
+    Node<T>* Minimum()
+    {
+        return MinimumFromNode(root);
+    }
+
+    // Función para obtener el valor mínimo del árbol.
+    T MinimumValue()
+    {
+        Node<T>* temp = Minimum();
+        if (temp != nullptr)
+        {
+            return temp->data;
+        }
+    }
+
+    // Función para encontrar el nodo con el valor máximo desde un nodo dado.
+    Node<T>* MaximumFromNode(Node<T>* in_root)
+    {
+        Node<T>* temp = in_root;
+        while (temp->right != nullptr)
+        {
+            temp = temp->right;
+        }
+        return temp;
+    }
+
+    // Función para encontrar el nodo con el valor máximo en todo el árbol.
+    Node<T>* Maximum()
+    {
+        return MaximumFromNode(root);
+    }
+
+    // Función para obtener el valor máximo del árbol.
+    T MaximumValue()
+    {
+        Node<T>* temp = Maximum();
+        if (temp != nullptr)
+        {
+            return temp->data;
+        }
+    }
+
+    // Función para buscar un valor en el árbol y devolver el nodo que lo contiene.
+    Node<T>* Search(T value)
+    {
+        Node<T>* temp = root;
+        while (temp != nullptr && temp->data != value)
+        {
+            if (value < temp->data)
+            {
+                temp = temp->left;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+        return temp;
+    }
+
+    // Función para insertar un valor en el árbol.
+    void Insert(T value)
+    {
+        Node<T>* temp = root;
+        Node<T>* temp_parent = nullptr;
+        while (temp != nullptr)
+        {
+            if (value < temp->data)
+            {
+                temp_parent = temp;
+                temp = temp->left;
+            }
+            else
+            {
+                temp_parent = temp;
+                temp = temp->right;
+            }
+        }
+
+        Node<T>* newNode = new Node<T>(value, temp_parent);
+        if (temp_parent == nullptr)
+        {
+            root = newNode;
+        }
+        else if (value < temp_parent->data)
+        {
+            temp_parent->left = newNode;
+        }
+        else
+        {
+            temp_parent->right = newNode;
+        }
+    }
+
+    // Función para encontrar el sucesor de un nodo dado.
+    Node<T>* Sucessor(Node<T>* in_x)
+    {
+        if (in_x->right != nullptr)
+        {
+            return MinimumFromNode(in_x->right);
+        }
+        else
+        {
+            Node<T>* current = in_x;
+            Node<T>* temp_parent = in_x->parent;
+            while (temp_parent != nullptr && in_x == temp_parent->left)
+            {
+                current = temp_parent;
+                temp_parent = current->parent;
+            }
+            return temp_parent;
+        }
+    }
+
+    // Función para encontrar el predecesor de un nodo dado.
+    Node<T>* Predecessor(Node<T>* in_x)
+    {
+        if (in_x->left != nullptr)
+        {
+            return MaximumFromNode(in_x->left);
+        }
+
+        Node<T>* temp_parent = in_x->parent;
+        while (temp_parent != nullptr && in_x == temp_parent->left)
+        {
+            in_x = temp_parent;
+            temp_parent = temp_parent->parent;
+        }
+
+        return temp_parent;
+    }
+
+    // Función recursiva para recorrer el árbol en orden (izquierda - raíz - derecha).
+    void InOrder(Node<T>* in_x)
+    {
+        if (in_x != nullptr)
+        {
+            InOrder(in_x->left);
+            std::cout << in_x->data << ", ";
+            InOrder(in_x->right);
+        }
+    }
+
+    // Función iterativa para recorrer el árbol en orden (izquierda - raíz - derecha).
+    void InOrderIterative()
+    {
+        std::stack<Node<T>*> nodeStack;
+        Node<T>* current = root;
+
+        while (current != nullptr || !nodeStack.empty())
+        {
+            while (current != nullptr)
+            {
+                nodeStack.push(current);
+                current = current->left;
+            }
+
+            current = nodeStack.top();
+            nodeStack.pop();
+
+            std::cout << current->data << ", ";
+
+            current = current->right;
+        }
+    }
+
+    // Función recursiva para recorrer el árbol en preorden (raíz - izquierda - derecha).
+    void PreOrder(Node<T>* in_x)
+    {
+        if (in_x != nullptr)
+        {
+            std::cout << in_x->data << ", ";
+            PreOrder(in_x->left);
+            PreOrder(in_x->right);
+        }
+    }
+
+    // Función iterativa para recorrer el árbol en preorden (raíz - izquierda - derecha).
+    void PreOrderIterative()
+    {
+        std::stack<Node<T>*> nodeStack;
+        if (root != nullptr)
+            nodeStack.push(root);
+
+        while (!nodeStack.empty())
+        {
+            Node<T>* current = nodeStack.top();
+            nodeStack.pop();
+
+            std::cout << current->data << ", ";
+
+            if (current->right != nullptr)
+                nodeStack.push(current->right);
+
+            if (current->left != nullptr)
+                nodeStack.push(current->left);
+        }
+    }
+
+    // Función recursiva para recorrer el árbol en postorden (izquierda - derecha - raíz).
+    void PostOrder(Node<T>* in_x)
+    {
+        if (in_x != nullptr)
+        {
+            PostOrder(in_x->left);
+            PostOrder(in_x->right);
+            std::cout << in_x->data << ", ";
+        }
+    }
+
+    // Función iterativa para recorrer el árbol en postorden (izquierda - derecha - raíz).
+    void PostOrderIterative()
+    {
+        std::stack<Node<T>*> nodeStack;
+        Node<T>* current = root;
+        Node<T>* lastVisited = nullptr;
+
+        while (current != nullptr || !nodeStack.empty())
+        {
+            if (current != nullptr)
+            {
+                nodeStack.push(current);
+                current = current->left;
+            }
+            else
+            {
+                Node<T>* temp = nodeStack.top();
+                if (temp->right != nullptr && temp->right != lastVisited)
+                {
+                    current = temp->right;
+                }
+                else
+                {
+                    std::cout << temp->data << ", ";
+                    lastVisited = temp;
+                    nodeStack.pop();
+                }
+            }
+        }
+    }
+
+    // Función para reemplazar un nodo en el árbol.
+    void Transplant(Node<T>* in_u, Node<T>* in_v)
+    {
+        if (in_u->parent == nullptr)
+        {
+            root = in_v;
+        }
+        else if (in_u == in_u->parent->left)
+        {
+            in_u->parent->left = in_v;
+        }
+        else
+        {
+            in_u->parent->right = in_v;
+        }
+        if (in_v != nullptr)
+        {
+            in_v->parent = in_u->parent;
+        }
+    }
+
+    // Función para eliminar un nodo del árbol.
+    void DeleteNode(Node<T>* in_z)
+    {
+        if (in_z->left == nullptr)
+        {
+            Transplant(in_z, in_z->right);
+        }
+        else if (in_z->right == nullptr)
+        {
+            Transplant(in_z, in_z->left);
+        }
+        else
+        {
+            Node<T>* Y = MinimumFromNode(in_z->right);
+            if (Y != in_z->right)
+            {
+                Transplant(Y, Y->right);
+                Y->right = in_z->right;
+                Y->right->parent = Y;
+            }
+            Transplant(in_z, Y);
+            Y->left = in_z->left;
+            Y->left->parent = Y;
+        }
+
+        delete in_z;
+    }
+};
+
+int main()
+{
+    BinaryTree<int> tree;
+
+    tree.Insert(5);
+    tree.Insert(3);
+    tree.Insert(8);
+    tree.Insert(2);
+    tree.Insert(4);
+    tree.Insert(7);
+    tree.Insert(9);
+
+    std::cout << "InOrder Recursive: ";
+    tree.InOrder(tree.GetRoot());
+    std::cout << std::endl;
+
+    std::cout << "InOrder Iterative: ";
+    tree.InOrderIterative();
+    std::cout << std::endl;
+
+    std::cout << "PreOrder Recursive: ";
+    tree.PreOrder(tree.GetRoot());
+    std::cout << std::endl;
+
+    std::cout << "PreOrder Iterative: ";
+    tree.PreOrderIterative();
+    std::cout << std::endl;
+
+    std::cout << "PostOrder Recursive: ";
+    tree.PostOrder(tree.GetRoot());
+    std::cout << std::endl;
+
+    std::cout << "PostOrder Iterative: ";
+    tree.PostOrderIterative();
+    std::cout << std::endl;
+
+    int value = 7;
+    typename BinaryTree<int>::Node<int>* node = tree.Search(value);
+    if (node != nullptr)
+    {
+        std::cout << "Found node with value " << value << std::endl;
+        typename BinaryTree<int>::Node<int>* predecessor = tree.Predecessor(node);
+        if (predecessor != nullptr)
+            std::cout << "Predecessor of " << value << " is " << predecessor->data << std::endl;
+        else
+            std::cout << "No predecessor for " << value << std::endl;
+    }
+    else
+    {
+        std::cout << "Node with value " << value << " not found" << std::endl;
+    }
+
+    return 0;
+}
